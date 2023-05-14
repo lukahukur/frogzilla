535117d637102aaa20c3cd9c1d3cd575cf55fa14
lukahukur <6wingman9@gmail.com>
2023-05-14 19:11:42 +0400

    camera pos fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, useRef, useState, useEffect } from "react";
-import { Canvas, useThree } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Vector3, DirectionalLight } from "three";
 import {
   OrbitControls,
@@ -12,27 +12,35 @@ import TWEEN from "@tweenjs/tween.js";
 import Menu from "./components/Menu.jsx";
 
 const pages = {
-  welcome: { name: "welcome", coords: [-8.1148, 1.6744, -4.9318] },
-  about: { name: "about", coords: [3.3127, 5.8648, 3.143] },
-  tokenomics: { name: "tokenomics", coords: [-5.4786, 1.0019, 1.5075] },
-  "Join the Voyage": { name: "Join the Voyage", coords: [-15.84, 2.8, 0.76] },
-  "Connect with Us": { name: "Connect with Us", coords: [-8.6, 4.01, 4.9] },
+  welcome: {
+    name: "welcome",
+    coords: [-8.1148, 1.6744, -4.9318],
+    lookAt: [0, 0, 0],
+  },
+  about: {
+    name: "about",
+    coords: [3.3127, 5.8648, 3.143],
+    lookAt: [0, 0, 0],
+  },
+  tokenomics: {
+    name: "tokenomics",
+    coords: [-5.4786, 1.0019, 1.5075],
+    lookAt: [0, 0, 0],
+  },
+  "Join the Voyage": {
+    name: "Join the Voyage",
+    coords: [-8, 1, -0.2],
+    lookAt: [-100, 2, 3],
+  },
+  "Connect with Us": {
+    name: "Connect with Us",
+    coords: [-8.6, 4.01, 4.9],
+    lookAt: [0, 0, 0],
+  },
 };
 
 let pageNum = 1;
 let lastPage = Object.keys(pages).length;
-
-const DURATION = 1000;
-
-const smoothAnimation = (position, endPosition) => {
-  const easing = TWEEN.Easing.Quadratic.InOut;
-
-  const tween = new TWEEN.Tween(position)
-    .to(endPosition, DURATION)
-    .easing(easing);
-
-  tween.start();
-};
 
 export default function App() {
   const [cameraPos, setCameraPos] = useState({ x: 0, y: 0, z: 0 });
@@ -40,29 +48,11 @@ export default function App() {
   const cameraRef = useRef();
   const [page, setPage] = useState(pages["welcome"]);
 
-  useEffect(() => {
-    let t = setTimeout(() => {
-      smoothAnimation(
-        cameraRef.current.position,
-        new Vector3(page.coords[0], page.coords[1], page.coords[2])
-      );
-    });
-    return () => clearTimeout(t);
-  }, [page]);
-
   const changeRoute = () => {
     if (pageNum >= lastPage) pageNum = 0;
     setPage(pages[Object.keys(pages)[pageNum]]);
     pageNum++;
   };
-
-  useEffect(() => {
-    const animate = () => {
-      requestAnimationFrame(animate);
-      TWEEN.update();
-    };
-    animate();
-  }, []);
 
   return (
     <div className="w-screen h-screen relative">
@@ -85,6 +75,7 @@ export default function App() {
           enablePan={true}
           enableRotate={true}
         />
+        <Controls pos={page} />
       </Canvas>
       <div
         className={`
@@ -99,4 +90,27 @@ export default function App() {
       </div>
     </div>
   );
+}
+const DURATION = 1000;
+
+const smoothAnimation = (position, endPosition) => {
+  const easing = TWEEN.Easing.Quadratic.InOut;
+
+  const tween = new TWEEN.Tween(position)
+    .to(endPosition, DURATION)
+    .easing(easing);
+
+  tween.start();
+};
+
+function Controls({ pos }) {
+  return useFrame((state) => {
+    // state.camera.position.set(pos.coords[0], pos.coords[1], pos.coords[2]);
+    state.camera.position.lerp(
+      { x: pos.coords[0], y: pos.coords[1], z: pos.coords[2] },
+      0.1
+    );
+    state.camera.lookAt(pos.lookAt[0], pos.lookAt[1], pos.lookAt[2]);
+    state.camera.updateProjectionMatrix();
+  });
 }
